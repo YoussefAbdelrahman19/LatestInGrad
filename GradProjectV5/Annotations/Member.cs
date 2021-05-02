37ffaa0ec19ef71f7a6b0e3693150f279b09b4ae
Youssef AbdElrahman <youssefabdelrahman290@yahoo.com>
2021-05-03 01:46:49 +0200

    5/3 edit

---
+++ GradProjectV5/Annotations/Member.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GradProjectV5.Models
+{
+    [MetadataType(typeof(MemberAnnotations))]
+    public partial class Member
+    {
+    }
+    public class MemberAnnotations
+    {
+        [Display(Name = "الاسم بالكامل")]
+        [Required(ErrorMessage = "يتطلب ادخال الاسم بالكامل")]
+        public string FullName { get; set; }
+
+        [Display(Name = "الرقم القومي")]
+        [RegularExpression(@"[0-9]{14}",ErrorMessage = "يرجي ادخل رقم قومي صحيح")]
+        [Required(ErrorMessage = "يتطلب ادخال الرقم القومي")]
+
+        public string NationalId { get; set; }
+
+        [RegularExpression(@"(01)[0-9]{9}", ErrorMessage = "يتطلب ادخال رقم هاتف صحيح ")]
+        [Required(ErrorMessage = "يتطلب ادخال رقم الهاتف ")]
+        [Display(Name = "رقم الهاتف")]
+        public string PhoneNo { get; set; }
+        [Display(Name = "العنوان")]
+        [Required(ErrorMessage = "يتطلب ادخال العنوان")]
+
+
+        public string Address { get; set; }
+        [Display(Name = "العمر")]
+        [Required(ErrorMessage = "يتطلب ادخال العمر ")]
+        
+        [RegularExpression(@"[0-9]{2}")]
+        public string Age { get; set; }
+    }
+}
---
+++ GradProjectV5/Annotations/Pharamacy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GradProjectV5.Models
+{
+    [MetadataType(typeof(PharamacyAnnotation))]
+
+    public partial class Pharamacy
+    {
+    }
+
+    public class PharamacyAnnotation
+    {
+        [Display(Name = "اسم الصيدلية")]
+        [Required(ErrorMessage = "يتطلب ادخال اسم الصيدلية ")]
+        public string PharmacyName { get; set; }
+        [Display(Name = "عنوان الصيدلية")]
+        [Required(ErrorMessage = "يتطلب ادخال عنوان الصيدلية ")]
+        public string PharmacyAddress { get; set; }
+        [Display(Name = "التليفون")]
+        [RegularExpression(@"(01)[0-9]{9}",ErrorMessage = "يتطلب ادخال رقم هاتف صحيح ")]
+        [Required(ErrorMessage = "يتطلب ادخال رقم الهاتف ")]
+        public string PharmacyPhone { get; set; }
+    }
+}
